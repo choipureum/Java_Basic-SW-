b3fa2e1d47f544395207b5ea77cc8fed133cbaff
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-01-30 18:16:44 +0900

    Create VariablesEx_06.java

---
+++ 1주차/Java01_variables/VariablesEx_06.java
@@ -0,0 +1,18 @@
+package Java01_variables;
+import java.util.*;
+//1.30(목)
+
+
+public class VariablesEx_06 {
+	public static void main(String[]args )
+	{
+		//문자열 변수 선언
+		String fruit="Apple";  //"" 큰따옴표로 구분, 문자열 상수를 저장(대입)
+		System.out.print("과일명: "+fruit);
+	}
+}
+
+//	<참조형 데이터>
+//	-기본 데이터타입과는 다른 방식
+// String 문자열 : 단일문자(char)들의 나열	
+// 문장을 표현하는 데이터 타입
