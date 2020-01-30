e6975dd89ad705551b45b5a4cc402b41134ba4d2
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-01-30 18:17:04 +0900

    Create VariablesQuiz.java

---
+++ 1주차/Java01_variables/VariablesQuiz.java
@@ -0,0 +1,17 @@
+package Java01_variables;
+
+public class VariablesQuiz {
+	public static void main(String [] args)
+	{
+		String name ="최푸름";
+		int age =27;
+		String gender ="남";
+		double height =172.2;
+		
+		System.out.println("이름: "+name);
+		System.out.println("나이: "+age);
+		System.out.println("성별: "+gender);
+		System.out.println("키: "+height);
+	
+	}
+}
