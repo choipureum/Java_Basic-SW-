fc64cc6dd7fbeb92ea72738d6f7ce4bc64cc095e
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-03 20:42:22 +0900

    Create if_01.java

---
+++ 2주차/Java04_control/condition/if_01.java
@@ -0,0 +1,14 @@
+package Java04_control.condition;
+import java.util.*;
+
+public class if_01 {
+	public static void main(String[] args) {
+		//조건문- if
+		//if(조건){}
+		//조건식은 연산의 결과가 true/false로 판별가능한 연산식
+		if(false) {
+			System.out.println("출력될까???");
+		}
+		System.out.println("여기는 if 바깥입니다");
+	}
+}
