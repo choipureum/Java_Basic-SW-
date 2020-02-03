63aff71a28fbeac309ebdd059df825fc988b26b1
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-03 20:46:01 +0900

    Update if_01.java

--- 2주차/Java04_control/condition/if_01.java
+++ 2주차/Java04_control/condition/if_01.java
@@ -10,5 +10,27 @@ public class if_01 {
 			System.out.println("출력될까???");
 		}
 		System.out.println("여기는 if 바깥입니다");
+		//		< 제어문, Control Statement >
+//		 -기본적인 프로그램의 정상적인 흐름을 제어하는 구문
+//		 (정상적인 흐름 : main() 메소드에서 시작하여 코드 한 줄 씩 끝가지 수행되는 것)
+//		 -분기한다라고도 함
+//		 -프로그램의 실행흐름을 개발자가 원하는 방향으로 제어
+//		 
+//		 < 제어문의 종류 >
+//		  +조건문,Condition
+//
+//			-if
+//			-else if
+//			-else
+//
+//		  +반복문,Loop
+//
+//			-for
+//			-while
+//			-do-while
+//
+//		  +선택문
+//
+//			-switch - case
 	}
 }
