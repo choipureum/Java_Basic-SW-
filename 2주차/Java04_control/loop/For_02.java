899b78e5397703f371d91065e288002a734e8d29
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-04 17:45:24 +0900

    Create For_02.java

---
+++ 2주차/Java04_control/loop/For_02.java
@@ -0,0 +1,16 @@
+package Java04_control.loop;
+import java.util.*;
+
+public class For_02 {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+		//i가 0부터 10보다 작은 동안에 1씩 증가하면서 반복한다
+		
+		//반복횟수 : 10번
+		//반복구간 : 0~9
+		
+	}
+}
