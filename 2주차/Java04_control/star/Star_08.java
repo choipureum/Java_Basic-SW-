b4a6d1cc9d51853565fe446849341b655a0da3f1
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-04 21:10:57 +0900

    Create Star_08.java

---
+++ 2주차/Java04_control/star/Star_08.java
@@ -0,0 +1,13 @@
+package Java04_control.star;
+
+public class Star_08 {
+	public static void main(String[] args) {
+
+		for(int i=0;i<5;i++) {
+			for(int j=i;j>=0;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+	}
+}
+}
