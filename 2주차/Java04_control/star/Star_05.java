6ae03718123880a7cb166e78071e5814964cb61b
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-04 21:04:39 +0900

    Create Star_05.java

---
+++ 2주차/Java04_control/star/Star_05.java
@@ -0,0 +1,13 @@
+package Java04_control.star;
+
+public class Star_05 {
+	public static void main(String[] args) {
+		
+		for(int i=1;i<6;i++) {
+			for(int j=1;j<6;j++) {
+				System.out.print(j);
+		}
+			System.out.println();
+	}
+}
+}
