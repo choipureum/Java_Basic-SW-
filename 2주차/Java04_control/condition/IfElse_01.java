209358fdc2f05e0f4c89a5444982b263fdbc75fa
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-04 16:36:57 +0900

    Create  IfElse_01.java

---
+++ 2주차/Java04_control/condition/IfElse_01.java
@@ -0,0 +1,24 @@
+package Java04_control.condition;
+import java.util.*;
+
+public class IfElse_01 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//제어문, else
+//		if(조건식) {
+			//조건식이 참일 때 수행하는 코드
+//		}
+//		else {
+			//조건식이 거짓일 때 수행하는 코드
+//		}
+		
+//      예시--------------------------------------
+		int num= 13;
+		if(num ==11) {
+			System.out.println("num은 11이당");
+		}
+		else {
+			System.out.println("num은 11이 아니다앙");
+		}
+	}
+}
