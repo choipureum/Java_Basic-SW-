4d6a914c9e0d48711a53041b85b3c3d6e097ba3f
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-05 17:20:13 +0900

    Create While_01.java

---
+++ 2주차/Java04_control/loop/While_01.java
@@ -0,0 +1,26 @@
+package Java04_control.loop;
+
+public class While_01 {
+	public static void main(String[] args) {
+		
+		while(true) {
+			System.out.println("HI");
+			//무한반복, 무한루프
+		}
+		//for문을 이용한 무한루프(사용 X)
+//		for(;;) {
+//			System.out.println("Hello");
+//		}
+	}
+}
+// 	< while(조건식){} >
+//	 +조건식이 참일 경우 반복 수행 코드
+//   +조건식이 거짓인 경우 탈출한다
+//   +조건식이 참일경우 {} 괄호를 수행하고 다시 조건식 검색
+//   while(true)를 많이 사용한다
+//		중단 강제종료 break; 사용
+//
+//
+//
+//
+//
