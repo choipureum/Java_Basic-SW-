c6d4a4f48d582131feff461da8d42c088e5a4528
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-05 19:44:11 +0900

    Create Switch_03.java

---
+++ 2주차/Java04_control/switchCase/Switch_03.java
@@ -0,0 +1,17 @@
+package Java04_control.switchCase;
+
+public class Switch_03 {
+	public static void main(String[] args) {
+		//switch의 비교대상으로는 정수로 표현되는 값이어야한다
+		
+		//switch에 double형,boolean형은 사용할 수 없다
+//		double pi =3.14;
+//		switch(pi)P
+//			case 3.14:  -->에러
+		char alpha ='b';
+		switch(alpha) {
+			case 'b':
+		}
+		//정수가 아닌 char값은 가능
+	}
+}
