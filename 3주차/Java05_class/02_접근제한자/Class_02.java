021a237f3d232f57d83a419d2bb22e025a2d69cd
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-10 21:28:51 +0900

    Create Class_02.java

---
+++ 3주차/Java05_class/02_접근제한자/Class_02.java
@@ -0,0 +1,17 @@
+package Java06_class;
+
+//기능 클래스
+public class Class_02 {
+
+	
+	//접근 제한자를 적용한 멤버 필드
+	private int num1 =10;
+	
+	int num2=20; //default : package 내에서만
+	
+	protected int num3=30;
+	
+	public int num4=40;
+		//멤버필드에 넣는 초기화 값들은 실제 데이터를 넣는 건 아니고 설계이다
+	
+}
