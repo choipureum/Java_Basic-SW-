b5a45c9f0b3d4373c1af09cb1fd86e8ff4912aec
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-10 20:36:49 +0900

    Create Class_01.java

---
+++ 3주차/Java05_class/01_concept/Class_01.java
@@ -0,0 +1,23 @@
+package Java06_class;
+import java.util.*;
+
+// 기능 클래스 : main()메소드가 없는 클래스 
+public class Class_01 {
+	
+	//멤버 필드(Member Field) --데이터, 정보, 속성
+	int num;
+	String str;
+	
+	//멤버 메소드(Member Method) --기능, 행위, 동작
+	public void display() {
+		System.out.println("메소드");
+	}
+}
+//메소드(Method)
+// + 클래스안에 들어가서 정의되는 기능(안쪽에 구현)
+
+
+//기능(Function)
+// +자바는 안쓴다
+// +함수의 의미(자바는 클래스)
+// +단순한 기계적인 일
