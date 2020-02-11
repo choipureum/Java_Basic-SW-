408e732ced2d37ac91f44b87933a7fc198b2ec27
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-11 17:59:25 +0900

    #method구성하기

---
+++ 3주차/Java05_class/04_Method/Method_01.java
@@ -0,0 +1,30 @@
+package Java06_class.method;
+import java.util.*;
+
+//기능 클래스1
+public class Method_01 {
+	
+	//2개의 정수를 덧셈해주는 기능	
+	public int add(int num1, int num2) {
+		int reult= num1+num2;
+		return reult;
+	}
+
+	//1. 메소드의 기본형태 작성
+	
+	//2. 메소드 이름 ->만들고자 하는 기능을 대표하는 이름으로 적절히 해보자 
+//	-> add로 작성
+	
+	//3. 매개변수를 작성해라 ->기능을 수행하는 데 필요한 입력 값을 저장하는 변수
+//	-> (int num1, int num2) : ,(comma)로 구분한다
+	
+	//4. 실행코드 작성 ->메소드가 수행할 기능을 작성, 구현
+//  -> int return=num1+num2 	
+	
+	//5. return 코드 작성 -> 수행 결과로 사용할 반환 값 작성
+//	-> 	return result;
+	
+	//6. return 데이터 타입 작성 ->return 코드 값의 데이터타입을 명시
+//	-> int
+	
+}
