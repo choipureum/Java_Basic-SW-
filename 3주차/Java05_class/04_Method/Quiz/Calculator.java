a8a4dd3e75a5336b98270620681f9eacb4cc6a54
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-11 19:44:15 +0900

    #계산기기능클래스

---
+++ 3주차/Java05_class/04_Method/Quiz/Calculator.java
@@ -0,0 +1,17 @@
+package Java06_class.method.quiz;
+
+import java.util.*;
+public class Calculator {
+	public int sum(int n1, int n2) {
+		return n1+n2;
+	}
+	public int sub(int n1, int n2) {
+		return n1-n2;
+	}
+	public double div(int n1, int n2) {
+		return (double)n1/n2;
+	}
+	public int mul(int n1, int n2) {
+		return n1*n2;
+	}
+}
