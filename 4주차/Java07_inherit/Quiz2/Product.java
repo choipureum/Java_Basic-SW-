8b2cbc0c67cf0462929a748f7794da3ea6513daf
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-18 20:43:33 +0900

    #정보저장 추상화클래스

--- 4주차/Java07_inherit/Quiz2/Product.java
+++ 4주차/Java07_inherit/Quiz2/Product.java
@@ -1,6 +1,8 @@
 package Java07_inherit.Quiz2;
 
-public class Product {
+//추상클래스(설계도 역할)이지만 건물은 못짓는다
+//Product p = new Product(); 이런 객체생성 할수 없다
+public abstract class Product {
 	private String model;
 	private int price;
 	
@@ -25,8 +27,7 @@ public class Product {
 	public  void setPrice(int price) {
 		this.price = price;
 	}
-	public void out() {
-		
-	}
+	//추상메소드(추상메소드는 일반클래스에서 사용할 수 없다)
+	public abstract void out(); //추상화하다(오버라이드해야한다)근데 몸체에서는 할게없다(추상메소드)
 	
 }
