e8199095ff7c7ef23ade2fd3a96b4693989566be
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-19 20:47:51 +0900

    #배열인덱스 퀴즈

---
+++ 4주차/Java08_abstract/Quiz2/ArrayEx.java
@@ -0,0 +1,33 @@
+package Java08_abstract.interfaceEx.Quiz2;
+
+import java.util.Arrays;
+
+public class ArrayEx {
+	public static void main(String[] args) {
+		Array ar = new Array();
+		
+//		ar.add("Apple");
+//		ar.add("Banana");
+//		ar.add("Cherry");
+
+		ar.add("Apple");
+		ar.add("Banana");
+		ar.add("Cherry");
+		ar.add("Pineapple");
+
+		ar.print(); // Apple, Banana, Cherry
+		System.out.println("----------");
+		
+		ar.remove("Banana");
+		ar.print(); // Apple, Cherry
+		System.out.println("----------");
+
+		System.out.println( ar.get(1) ); // Cherry
+		System.out.println( ar.get(2) ); // null
+		System.out.println("----------");
+		
+		System.out.println( ar.find("Apple") ); // 0
+		System.out.println( ar.find("Banana") ); // -1
+		System.out.println("----------");
+	}
+}
