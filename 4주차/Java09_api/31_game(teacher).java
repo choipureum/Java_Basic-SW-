509200ba762948f6cb6d471decd9220ccd6e1418
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-21 08:41:09 +0900

    #배스킨 31게임(강사님코드)

--- 4주차/Java09_api/31_game(teacher).java
+++ 4주차/Java09_api/31_game(teacher).java
@@ -3,7 +3,7 @@ package java09_api;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Baskin {
+public class Baskin31 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Random ran = new Random();
