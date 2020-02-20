43d30865224ac02d6e833d8d890498fed10cb847
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-20 21:30:26 +0900

    #강사님 코드

---
+++ 4주차/Java09_api/31_game(teacher).java
@@ -0,0 +1,53 @@
+package java09_api;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Baskin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random ran = new Random();
+
+		int com=0; //컴퓨터의 수
+		int user=0; //사용자의 수
+		
+		int num = 1;
+		
+		int max = 31;
+		
+		int winner = 0;
+		
+		while(num<=max) {
+			com = ran.nextInt(3) + 1;
+			
+			for(int i=0; i<com && num<=max; i++) {
+				System.out.println("com : " + num++);
+				
+				if(num>max)	winner=2;
+			}
+			
+			while(num<=max) {
+				System.out.println();
+				System.out.print(">> 몇 개의 연속된 숫자를 말하시겠습니까? ");
+				user = sc.nextInt();
+
+				if(user<1 || user>3) {
+					System.out.println();
+					System.out.println(">> 1~3까지만 입력가능합니다. 다시!");
+				} else	break;
+			}
+			
+			for(int i=0; i<user && num<=max; i++) {
+				System.out.println("user : " + num++);
+				if(num>max)	winner=1;
+			}
+		}
+		
+		if(winner==1) {
+			System.out.println("컴 승리");
+		} else if(winner==2) {
+			System.out.println("유저 승리");
+		}
+	}
+}
+
