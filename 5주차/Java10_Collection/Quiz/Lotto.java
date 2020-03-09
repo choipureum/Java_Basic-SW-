964ce34a79cb94852b0d71ce59fcca16363d258b
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-03-09 20:34:40 +0900

    #TreeSet

--- 5주차/Java10_Collection/Quiz/Lotto.java
+++ 5주차/Java10_Collection/Quiz/Lotto.java
@@ -16,6 +16,10 @@ public class Lotto {
 		}
 		List list = new ArrayList(set);
 		System.out.println(list);
+		
+		//방법2 : 배열로 변환하기
+		Object [] arr = set.toArray();
+		System.out.println(Arrays.toString(arr));
 	}
 }
 //중복안되는 45까지의 6개 난수발생과 자동 오름차순되는 TreeSet에 넣어서 이후 이를 리스트에 넣어주면 
