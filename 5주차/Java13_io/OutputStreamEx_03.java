8ed5626dad12088abd7666ce55d1c7754428641d
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-03-13 19:16:39 +0900

    #OutputStream-byte[]

---
+++ 5주차/Java13_io/OutputStreamEx_03.java
@@ -0,0 +1,37 @@
+package Java13_io.OutputStream;
+import java.io.*;
+
+public class OutputStreamEx_03 {
+	public static void main(String[] args) {
+		
+		OutputStream os = System.out;
+		byte[] b = new byte[1024];
+		int len=0; //의미있는 데이터의 길이(빈칸 제외)
+		
+		b[len++] = 'A';
+		b[len++] = 'p';
+		b[len++] = 'p';
+		b[len++] = 'l';
+		b[len++] = 'e';
+		b[len++] = '\n';
+		b[len++] = 'H';
+		b[len++] = 'i';
+		b[len++] = '\n';
+		
+		try {
+			os.write(b,0,len);
+			os.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {		
+				try {
+					if(os!=null) {
+					os.close();
+					}
+				} catch (IOException e) {					
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
