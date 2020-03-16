b78d55b5063a4d83c1303a7444e8bdf2b22733cb
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-03-16 21:22:57 +0900

    #FileCopy : charStream

--- 5주차/Java13_io/filterStream/charStream/FileCopy.java
+++ 5주차/Java13_io/filterStream/charStream/FileCopy.java
@@ -12,13 +12,14 @@ public class FileCopy {
 		PrintWriter bw =null;
 		//입출력 보조 변수
 		int total=0; //입력데이터의 총 길이(문자 수)
+		//파일복사에서 문자스트림을 쓰면 오차가 발생한다
 		
 		try {
-			br= new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			bw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file2))),true);
+			br= new BufferedReader(new FileReader(file)); //file위치에 "경로/이름"을 넣어줘도 된다
+			bw = new PrintWriter(new BufferedWriter(new FileWriter(file2)),true); //autoFlush
 			
 			while((str = br.readLine())!=null) {
-				total+=str.getBytes().length;
+				total+=str.getBytes().length; //복사된 문자바이트 개수
 				bw.println(str);
 			}
 			System.out.println("파일의 크기 : "+total+"Bytes");
