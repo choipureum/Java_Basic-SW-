d6afd5faadeac2d86c9f45aee8883d193ad49296
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-03-18 21:44:06 +0900

    #덧셈결과를 출력

--- 5주차/Java15_thread/Sync/Output.java
+++ 5주차/Java15_thread/Sync/Output.java
@@ -11,8 +11,20 @@ public class Output extends Thread{
 	public void run() {
 		System.out.println("---Output Start---");
 		synchronized(data) {
-			System.out.println("Total : "+data.total);		
-		}		
+			if(!data.available) {
+				//덧셈이 완료되지 않은상황
+				try {
+					data.wait();//그냥 wait()을 하게되면 자기자신을 기다리는것이다(data)를 기다리는 것이므로
+					// data.를 기다려야한다 //LOCK반납
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			
+			}else {
+				System.out.println("Total : "+data.total);	
+		
+			}	
+		}
 		System.out.println("---Output   end---");
 		
 	}
