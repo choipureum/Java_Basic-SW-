6019b4c2ea556fc09165c5e6abd9d91857534282
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-03-18 21:44:37 +0900

    #sync 결과저장 , flag

--- 5주차/Java15_thread/Sync/Input.java
+++ 5주차/Java15_thread/Sync/Input.java
@@ -10,15 +10,16 @@ public class Input extends Thread{
 	@Override
 	public void run() {
 		//동기화를 하게되면 순서는 지켜진다
+		System.out.println("---Input Start---");
 		synchronized(data) {
-			//1부터 100까지 덧셈
-			System.out.println("---Input Start---");
-			
+			//1부터 100까지 덧셈	
 			for(int i=1;i<=100;i++) {
 				data.total+=i;
-			}
-			System.out.println("---Input   end---");
+			}			
+			data.available=true; //데이터 입력(덧셈)완료 상태
+			data.notifyAll();
+			//data.notify(); //둘다가능 ->wait()을 풀어준다
 		}	
-	
+		System.out.println("---Input   end---");
 	}
 }
